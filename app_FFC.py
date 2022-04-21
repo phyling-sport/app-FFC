47b1b0cbcf12090611e8e31dacf54e385df788ba
258498 <81810759+258498@users.noreply.github.com>
2022-04-21 14:40:52 +0200

    Update app_FFC.py

--- app_FFC.py
+++ app_FFC.py
@@ -47,9 +47,24 @@ if groupe == 'phyling':
 
         st.write(dirname)
         st.write(filename)
-        
-        fs = s3fs.S3FileSystem(anon=True)
-        st.write(fs.ls('phyling'))
+
+        # Create connection object.
+        # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
+        fs = s3fs.S3FileSystem(anon=False)
+
+        # Retrieve file contents.
+        # Uses st.experimental_memo to only rerun when the query changes or after 10 min.
+        @st.experimental_memo(ttl=600)
+        def read_file(filename):
+            with fs.open(filename) as f:
+                return f.read().decode("utf-8")
+
+        content = read_file("phyling/res_Thomas.csv")
+
+        # Print results.
+        for line in content.strip().split("\n"):
+            name, pet = line.split(",")
+            st.write(f"{name} has a :{pet}:")
 
 #         with fs.open('phyling/res_Thomas.csv', 'rb') as f:
 #             print(f.read())
