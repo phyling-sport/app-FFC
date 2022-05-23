52b7b565147119014ae2f002a181716f0affe0e1
258498 <81810759+258498@users.noreply.github.com>
2022-05-23 13:52:41 +0200

    Update app_FFC.py

--- app_FFC.py
+++ app_FFC.py
@@ -173,6 +173,7 @@ if groupe == 'phyling':
                 st.warning("Attention, l'écart à la moyenne n'est pas significatif")
                 st.write('p_value = ',res[1].round(2))
             col1, col2 = st.columns([0.8,0.2])
+            col3, col4 = st.columns(2)
             with col1:
                 st.write('')
                 st.subheader("Analyse statistique de l'évolution")
@@ -191,7 +192,6 @@ if groupe == 'phyling':
                     if len(df[df['athlete_name']==sujet_list].loc[end_t][val].values) != len(df[df['athlete_name']==sujet_list].loc[start_t][val].values):
                         st.write('échantillons de taille inégale')
                     else :
-                        col3, col4 = st.columns(2)
                         res1=stats.ttest_rel(df[df['athlete_name']==sujet_list].loc[end_t][val].values,
                                              df[df['athlete_name']==sujet_list].loc[start_t][val].values)
 
