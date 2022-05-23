22989b8315e33b9a8d2cbf8efb79cca76d0f334f
258498 <81810759+258498@users.noreply.github.com>
2022-05-23 13:32:03 +0200

    Update app_FFC.py

--- app_FFC.py
+++ app_FFC.py
@@ -179,7 +179,7 @@ if groupe == 'phyling':
 
                 start = df[df['athlete_name']==sujet_list]['date'].unique()[0]
                 end = df[df['athlete_name']==sujet_list]['date'].unique()[-1]
-                
+                col3, col4 = st.columns(2)
                 if start == end : 
                     st.write("L'athlete n'a pas assez de données pour l'analyse statistique")
                 else:
@@ -189,13 +189,12 @@ if groupe == 'phyling':
                                                        value=(start, end))
                 
                     df.set_index('date',inplace=True)
-                col3, col4 = st.columns(2)
+               
                     if len(df[df['athlete_name']==sujet_list].loc[end_t][val].values) != len(df[df['athlete_name']==sujet_list].loc[start_t][val].values):
                         st.write('échantillons de taille inégale')
                     else :
                         res1=stats.ttest_rel(df[df['athlete_name']==sujet_list].loc[end_t][val].values,
                                              df[df['athlete_name']==sujet_list].loc[start_t][val].values)
-
 
                         col3.metric(val + ' moyenne '+start_t,
                                     np.round(df[df['athlete_name']==sujet_list].loc[start_t][val].mean(),decimals=2),
