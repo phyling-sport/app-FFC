cdafcdb14944272a92b56cb08a741e0a9ec146e2
258498 <81810759+258498@users.noreply.github.com>
2022-05-23 13:29:02 +0200

    Update app_FFC.py

--- app_FFC.py
+++ app_FFC.py
@@ -189,25 +189,29 @@ if groupe == 'phyling':
                                                        value=(start, end))
                 
                     df.set_index('date',inplace=True)
-                    res1=stats.ttest_rel(df[df['athlete_name']==sujet_list].loc[end_t][val].values,
-                                         df[df['athlete_name']==sujet_list].loc[start_t][val].values)
-                
-                    col3, col4 = st.columns(2)
+                    
+                    if len(df[df['athlete_name']==sujet_list].loc[end_t][val].values) != len(df[df['athlete_name']==sujet_list].loc[start_t][val].values):
+                        st.write('échantillons de taille inégale')
+                    else :
+                        res1=stats.ttest_rel(df[df['athlete_name']==sujet_list].loc[end_t][val].values,
+                                             df[df['athlete_name']==sujet_list].loc[start_t][val].values)
 
-                    col3.metric(val + ' moyenne '+start_t,
-                                np.round(df[df['athlete_name']==sujet_list].loc[start_t][val].mean(),decimals=2),
-                                delta='', delta_color="normal")
-                    col4.metric(val + ' moyenne '+end_t,
-                                np.round(df[df['athlete_name']==sujet_list].loc[end_t][val].mean(),decimals=2),
-                                delta=np.round(df[df['athlete_name']==sujet_list].loc[end_t][val].mean()-df[df['athlete_name']==sujet_list].loc[start_t][val].mean(),decimals=2), delta_color="normal")
+                        col3, col4 = st.columns(2)
 
-                    col1, col2 = st.columns([0.8,0.2])
-                    with col1:
-                        if res1[1]<0.05:
-                            st.success('écart significatif à la moyenne du groupe')
-                        else:
-                            st.warning("Attention, l'écart à la moyenne n'est pas significatif")
-                            st.write('p_value = ',res1[1].round(2))
+                        col3.metric(val + ' moyenne '+start_t,
+                                    np.round(df[df['athlete_name']==sujet_list].loc[start_t][val].mean(),decimals=2),
+                                    delta='', delta_color="normal")
+                        col4.metric(val + ' moyenne '+end_t,
+                                    np.round(df[df['athlete_name']==sujet_list].loc[end_t][val].mean(),decimals=2),
+                                    delta=np.round(df[df['athlete_name']==sujet_list].loc[end_t][val].mean()-df[df['athlete_name']==sujet_list].loc[start_t][val].mean(),decimals=2), delta_color="normal")
+
+                        col1, col2 = st.columns([0.8,0.2])
+                        with col1:
+                            if res1[1]<0.05:
+                                st.success('écart significatif à la moyenne du groupe')
+                            else:
+                                st.warning("Attention, l'écart à la moyenne n'est pas significatif")
+                                st.write('p_value = ',res1[1].round(2))
                              
         elif analyse == 'Comparaisons de courbes':
             df_courbes=pd.DataFrame()
